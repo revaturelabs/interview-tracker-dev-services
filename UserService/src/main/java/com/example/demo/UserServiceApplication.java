1a69816baab6def3a352c1c90178e067f3eb9aab
brianabajah <abajahbrian@gmail.com>
2019-11-12 13:35:27 -0500

    First Commit

--- UserService/src/main/java/com/example/demo/UserServiceApplication.java
+++ UserService/src/main/java/com/example/demo/UserServiceApplication.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableFeignClients
 @SpringBootApplication
 public class UserServiceApplication {
 
