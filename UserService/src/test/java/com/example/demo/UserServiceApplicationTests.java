553e558cb8b2d347140d20026959ebe0ed9516cd
John Kelly <JohnThaddeusKelly@outlook.com>
2019-11-12 16:22:20 -0500

    feign test case

--- UserService/src/test/java/com/example/demo/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/example/demo/UserServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
+	private UserClient uc;
 	@Test
 	void contextLoads() {
+		User user = uc.getUser(0);
+		assertEquals("Username", user.getUsername());
 	}
 
 }
