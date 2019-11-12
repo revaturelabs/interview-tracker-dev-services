99080d57e1eecc7e7d5c9d6442625bbc91be45b0
github username <tinnin.brittany.n@gmail.com>
2019-11-12 14:25:08 -0500

    added annotations to interface

--- UserService/src/main/java/com/example/demo/UserClient.java
+++ UserService/src/main/java/com/example/demo/UserClient.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import feign.RequestLine;
+
 @FeignClient("users")
 public interface UserClient {
 
@@ -24,13 +26,17 @@ public interface UserClient {
 //	    @RequestMapping(method = RequestMethod.POST, value = "/stores/{storeId}", consumes = "application/json")
 //	    Store update(@PathVariable("storeId") Long storeId, Store store);
 	
+	@RequestLine("GET /users")
 	List<User> findAll();
 	
 //	User findById();
+	@RequestLine("GET /users/{id}")
 	User getUser(@PathVariable("id") int id);
 	
+	@RequestLine("POST")
 	void create(User user);
 	
-	User update(User user);
+	@RequestLine("POST /users/{id}")
+	User update(@PathVariable("id") int id, User user);
 	
 }
