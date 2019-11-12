2ecb0d32275969b8ba77b531644725e24e2caa5d
joefdavis <davisfranklinjoseph@gmail.com>
2019-11-12 14:37:52 -0500

    please lawd

--- UserService/src/main/java/com/example/demo/UserServiceApplication.java
+++ UserService/src/main/java/com/example/demo/UserServiceApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
