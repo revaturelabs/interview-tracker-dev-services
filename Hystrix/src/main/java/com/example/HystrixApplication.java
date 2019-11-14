d4ff8b6afaa02ab07303fb942c5fab9ebd9dd41a
joefdavis <davisfranklinjoseph@gmail.com>
2019-11-14 14:50:18 -0500

    m

--- Hystrix/src/main/java/com/example/HystrixApplication.java
+++ Hystrix/src/main/java/com/example/HystrixApplication.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableHystrix
+@EnableCircuitBreaker
 public class HystrixApplication {
 
 	public static void main(String[] args) {
