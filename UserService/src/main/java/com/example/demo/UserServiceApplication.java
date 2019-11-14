acf3e5a106c8bd8fea6c4d31fa5c8b16c55ef634
wliederer <williamliederer@gmail.com>
2019-11-14 13:35:15 -0500

    trying to fix

--- UserService/src/main/java/com/example/demo/UserServiceApplication.java
+++ UserService/src/main/java/com/example/demo/UserServiceApplication.java
@@ -1,18 +1,14 @@
 package com.example.demo;
 
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 
 
-<<<<<<< HEAD
-@EnableEurekaClient
 
-=======
->>>>>>> brittany
+
 @SpringBootApplication
 public class UserServiceApplication {
 
