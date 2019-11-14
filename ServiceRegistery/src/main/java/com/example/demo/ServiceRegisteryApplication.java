d00a0322d20f16e9d68656094dcdf4fd7b91fa56
wliederer <williamliederer@gmail.com>
2019-11-14 15:51:14 -0500

    please

--- ServiceRegistery/src/main/java/com/example/demo/ServiceRegisteryApplication.java
+++ ServiceRegistery/src/main/java/com/example/demo/ServiceRegisteryApplication.java
@@ -8,10 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> brittany
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegisteryApplication {
