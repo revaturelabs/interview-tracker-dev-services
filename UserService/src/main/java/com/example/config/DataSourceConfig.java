7ed2e878805a8bb9c41122705be02007d5e47747
github username <tinnin.brittany.n@gmail.com>
2019-11-14 10:40:52 -0500

    update

--- UserService/src/main/java/com/example/config/DataSourceConfig.java
+++ UserService/src/main/java/com/example/config/DataSourceConfig.java
@@ -2,9 +2,7 @@ package com.example.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class DataSourceConfig 
