84d9c0fa8ae7b658703efc2772794d658f86ade4
github username <tinnin.brittany.n@gmail.com>
2019-11-15 13:10:19 -0500

    added javadoc annotations

--- ServiceRegistery/src/main/java/com/example/demo/ServiceRegisteryApplication.java
+++ ServiceRegistery/src/main/java/com/example/demo/ServiceRegisteryApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 
+ * @author Jonghyun Park
+ *
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegisteryApplication {
--- UserService/src/main/java/com/example/demo/UserServiceApplication.java
+++ UserService/src/main/java/com/example/demo/UserServiceApplication.java
@@ -4,10 +4,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
-
+/**
+ * 
+ * @author Brittany Tinnin
+ *
+ */
 
 @SpringBootApplication
 public class UserServiceApplication {
